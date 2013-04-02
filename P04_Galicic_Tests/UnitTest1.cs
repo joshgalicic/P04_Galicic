77a8d6c98aa400c394dd3ac7d285f4c1d61901e1
Josh Galicic <joshgalicic@gmail.com>
2013-04-01 20:51:23 -0400

    added unit tests

--- P04_Galicic_Tests/UnitTest1.cs
+++ P04_Galicic_Tests/UnitTest1.cs
@@ -8,12 +8,53 @@ namespace P04_Galicic_Tests
 	public class UnitTest1
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void roomHasFourNullDoorsAtStart()
 		{
-			Wumpus test = new Wumpus();
+			Room room = new Room();
+			Assert.AreEqual(-1, room.door1);
+			Assert.AreEqual(-1, room.door2);
+			Assert.AreEqual(-1, room.door3);
+			Assert.AreEqual(-1, room.door4);
 
+			//Assert.AreEqual(true,test.truth());
+		}
+		
+		[TestMethod]
+		public void playerStartsinRoomZero()
+		{
+			Room.setupRooms();
+			Player player1 = new Player();
 
-			Assert.AreEqual(true,test.truth());
+			Assert.AreEqual(player1.currentRoom, Room.roomList[0]);
+		}
+
+		[TestMethod]
+		public void playerMovesToAnotherRoom()
+		{
+			Room.setupRooms();
+			Player player1 = new Player();
+
+			player1.move(Room.roomList[1]);
+
+			Assert.AreEqual(player1.currentRoom, Room.roomList[1]);
+
+		}
+
+		[TestMethod]
+		public void roomsAllLinkTogether()
+		{
+			Room.setupRooms();
+			Room.scrambleLinks();
+
+			foreach (Room room in Room.roomList)
+			{
+				Assert.AreNotEqual(room.door1, -1);
+				
+				Assert.AreNotEqual(room.door2, -1);
+				Assert.AreNotEqual(room.door3, -1);
+				Assert.AreNotEqual(room.door4, -1);
+
+			}
 		}
 	}
 }
